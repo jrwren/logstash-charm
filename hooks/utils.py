92996859ce01fac013f720a130acc64196512db2
Charles Butler <chuck@dasroot.net>
2014-09-10 02:03:45 -0400

    Massive method refactoring checkin of utils.py

--- hooks/utils.py
+++ hooks/utils.py
@@ -1,124 +1,95 @@
 #!/usr/bin/python
-import json
 import os
-import socket
 import subprocess
 import sys
-import pdb
+import shlex
+import shutil
 
+from charmhelpers.core.host import service, adduser, mkdir, chownr
+from charmhelpers.core.fetch import apt_update, apt_install
+from charmhelpers.core.fetch.archiveurl import download_and_validate
 from charmhelpers.core.hookenv import (
     Hooks,
     relation_get,
     relation_ids,
+    relation_list,
+    open_port,
+    close_port,
+    config,
 )
 
-from charmhelpers.core.host import (
-    service
-)
+# Constants
 
-from charmhelpers.core.fetch import (
-    apt_update,
-    apt_install
-)
-
-LS_URL = ("https://download.elasticsearch.org/"
-          "logstash/logstash/logstash-1.4.1.tar.gz")
+LS_USER = 'logstash'
+LS_URL = config()['logstash_url']
 LS_FNAME = os.path.basename(LS_URL)
-LS_DIR = "/opt/logstash"
-LS_CONF_DIR = "/etc/logstash.d"
+LS_DIR = os.path.join(os.path.sep, "opt", "logstash")
+LS_CONF_DIR = os.path.join(os.path.sep, "etc", "logstash.d")
+LS_LOG = os.path.join(os.path.sep, "var", "log", "logstash.log")
 
 hooks = Hooks()
+cfg = config()
 
 @hooks.hook('install')
 def install():
-    packages = ['openjdk-7-jre-headless', 'python-jinja2']    
+    packages = ['openjdk-7-jre-headless', 'python-jinja2']
+    apt_update()
+    apt_install(packages, FATAL=True)
 
+    uid = adduser(LS_USER).pw_uid
 
-    if not os.path.exists(LS_DIR):
-        os.mkdir(LS_DIR)
+    mkdir(LS_DIR, LS_USER)
+    mkdir(LS_CONF_DIR, LS_USER)
 
-    ls_tgz = os.path.join(
-        os.environ['CHARM_DIR'], 'files', LS_FNAME)
+    # touch the log
+    with open(LS_LOG, 'a'):
+        os.utime(LS_LOG, None)
+    os.chown(LS_LOG, uid)
 
-    if not os.path.exists(ls_tgz):
-        ls_tgz = os.path.join('/tmp', LS_FNAME)
-        subprocess.check_call(['curl', '-o', ls_tgz, LS_URL])
-        subprocess.check_call(
-            ['md5sum', '-c', os.path.join(
-                os.environ['CHARM_DIR'], 'files', 'checksums')])
-
-    subprocess.check_call([
-        'tar', '-xzv', '--strip-components=1',
-        '-C', "/opt/logstash", '-f', ls_tgz])
-
+    conf_src = os.path.join(os.environ['CHARM_DIR'], 'files', 'logstash.conf')
+    conf_dest = os.path.join(os.path.sep, 'etc', 'init', 'logstash.conf')
+    shutil.copyfile(conf_src, conf_dest)
 
 @hooks.hook('config-changed')
 def config_changed():
+
+    ls_tgz = os.path.join(os.environ['CHARM_DIR'], 'files', LS_FNAME)
+
+    # sorry if you reboot the server, we're fetching it again unless
+    # you fatpacked the charm. YOLO DORITOS
+    if not os.path.exists(ls_tgz):
+        link = cfg['logstash_url']
+        sum = cfg['logstash_sum']
+        tpath = download_and_validate(link, sum)
+
+        cmd = 'tar -xvz --strip-components=1 -C /opt/logstash -f {}'
+        subprocess.check_call(shlex.split(cmd.format(tpath)))
+
     service('restart', 'logstash-indexer')
 
+
+
+@hooks.hook('start')
+def start():
+  open_port(9300)
+
+@hooks.hook('stop')
+def stop():
+  close_port(9300)
+
 @hooks.hook('client-relation-joined')
-def get_elasticsearch():
+def client_joined():
     # Only supports a single scale unit (no scale-out of logstash-indexer)
     rid = relation_ids('client')[0]
     unit = relation_list(rid)[0]
-    
-    hookenv.relation_get('host', rid, unit)
-    
-    write_file(
-        '/etc/logstash.d/70-juju-outputs.conf',
-        'outputs.conf.template',
-        dict(elasticsearch=outputs_es))
-    
 
+    relation_get('host', rid, unit)
 
-##########
-## generic helpers
-
-class AttrDict(dict):
-
-    def __getattr__(self, key):
-        try:
-            return self[key]
-        except KeyError:
-            raise AttributeError(key)
-
-
-def write_file(path, template, data):
-    """Render the named jinja 'template' with the given template 'data' to
-    'path'. Return true if the contents of the path changed.
-    """
-    template = get_template(template)
-    content = template.render(**data)
-    if os.path.exists(path):
-        with open(path) as fh:
-            config = fh.read()
-            if config == content:
-                return False
-    with open(path, 'w') as fh:
-        fh.write(content)
-    return True
-
-
-def get_template(name):
-    template_path = os.path.join(os.environ['CHARM_DIR'], 'files', name)
-    with open(template_path) as fh:
-        content = fh.read()
-    return Template(content)
-
-
-def get_open_port(host=""):
-    """Get an open port on the machine.
-    """
-    temp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    temp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    temp_sock.bind((host, 0))
-    port = temp_sock.getsockname()[1]
-    temp_sock.close()
-    del temp_sock
-    return port
-
+    # write_file(
+    #     '/etc/logstash.d/70-juju-outputs.conf',
+    #     'outputs.conf.template',
+    #     dict(elasticsearch=outputs_es))
 
 if __name__ == "__main__":
     # execute a hook based on the name the program is called by
     hooks.execute(sys.argv)
-
